cc61f6d7b1b49cbcd3e538f7124c1b11fc03917e
sergeykraynov <kraynov.sergey@gmail.com>
2024-05-31 09:43:17 +0300

    Fix scss

--- src/js/modules/toggle.js
+++ src/js/modules/toggle.js
@@ -1,7 +1,8 @@
 const toggle = document.querySelectorAll('[data-toggle]');
 if (toggle.length > 0) {
     toggle.forEach(item => {
-        item.addEventListener('click', () => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
             item.classList.toggle('_active');
         });
     });
@@ -10,7 +11,8 @@ if (toggle.length > 0) {
 const toggle_target = document.querySelectorAll('[data-toggle-target]');
 if (toggle_target.length > 0) {
     toggle_target.forEach(item => {
-        item.addEventListener('click', () => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
             let target = document.querySelector(item.getAttribute('data-toggle-target'));
             if (target) target.classList.toggle('_active');
         });
